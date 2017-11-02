2974483066c0b2e5b8808f43f8837c75f6247dcb
Daniel Sørensen <1058493@ucn.dk>
2017-11-02 17:50:20 +0100

    tried some new things like directives

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
 @Component({
   selector: "app-server",
-  templateUrl: "./server.component.html"
+  templateUrl: "./server.component.html",
+  styles: [
+    `
+  .online{
+    color: white;
+  }
+    `
+  ]
 })
 export class ServerComponent {
   serverId = 10;
@@ -10,8 +17,15 @@ export class ServerComponent {
   getServerStatus() {
     return this.serverStatus;
   }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+  }
 
   getServerId() {
     return this.serverId;
+  }
+
+  getColor() {
+    return this.serverStatus === "online" ? "green" : "red";
   }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = "No Server was created!";
   serverName = "Testserver";
   serverCreated = false;
+  servers = ["Testserver", "Testserver 2"];
 
   constructor() {
     setTimeout(() => {
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       "Server was created! Name is " + this.serverName;
   }
